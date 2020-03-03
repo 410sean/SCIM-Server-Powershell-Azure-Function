d8392804802811804e322bd9b1f41b06e1e67195
410sean <410sean@gmail.com>
2020-03-03 13:10:37 -0500

    work on get users

--- UsersEndpoint/run.ps1
+++ UsersEndpoint/run.ps1
@@ -24,11 +24,31 @@ else {
     switch($Request.method){
         "GET"{
             if ($Request.params.path){
-                $body=@(Get-Content $inputTable -Raw | ConvertFrom-Json) | where-object{$_.username -eq $Request.params.path} | Select-Object $schema
+                $user=@(Get-Content $inputTable -Raw | ConvertFrom-Json) | where-object{$_.id -eq $Request.params.path} | Select-Object $schema
+                $user | Add-Member -NotePropertyName schemas -NotePropertyValue [pscustomobject]@("urn:ietf:params:scim:schemas:core:2.0:User")
+                    $user | Add-Member -NotePropertyName meta -NotePropertyValue ([PSCustomObject]@{
+                        resourceType = "User"
+                        location = "Users/$($user.id)"
+                    })
+                $body=$user
             }else{
                 #get all users (pagination)
-                $body=Get-Content $inputTable -Raw | ConvertFrom-Json
-            }
+                $resources=Get-Content $inputTable -Raw | ConvertFrom-Json
+                foreach ($user in $resources){
+                    $user | Add-Member -NotePropertyName schemas -NotePropertyValue [pscustomobject]@("urn:ietf:params:scim:schemas:core:2.0:User")
+                    $user | Add-Member -NotePropertyName meta -NotePropertyValue ([PSCustomObject]@{
+                        resourceType = "User"
+                        location = "Users/$($user.id)"
+                    })
+                }
+                $body=[pscustomobject]@{
+                    resources = $resources
+                    startIndex= 1
+                    itemsPerPage= $resources.count
+                    totalResults= $resources.count
+                    schemas= @("urn:ietf:params:scim:api:messages:2.0:ListResponse")
+                }    
+            }   
         }
         "POST"{
             #create user
