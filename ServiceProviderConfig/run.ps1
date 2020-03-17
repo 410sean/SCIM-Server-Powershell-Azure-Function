2c58d39edb254e5bf0122fd81dd8b8493952197f
410sean <410sean@gmail.com>
2020-03-17 10:54:04 -0400

    new version of spc

--- ServiceProviderConfig/run.ps1
+++ ServiceProviderConfig/run.ps1
@@ -1,67 +1,50 @@
 using namespace System.Net
 
 # Input bindings are passed in via param block.
-param($Request, $TriggerMetadata)
+param($Request, $TriggerMetadata, $ServiceProviderConfig, $authenticationSchemes)
 
 # Write to the Azure Functions log stream.
 Write-Host "PowerShell HTTP trigger function processed a request."
-
-# Interact with query parameters or the body of the request.
-$name = $Request.Query.Name
-if (-not $name) {
-    $name = $Request.Body.Name
-}
-
-if ($name) {
-    $status = [HttpStatusCode]::OK
-    $body = "Hello $name"
-}
-else {
-    $status = [HttpStatusCode]::BadRequest
-    $body = "Please pass a name on the query string or in the request body."
-}
 $status = [HttpStatusCode]::OK
-$body='{
-    "schemas": [
-      "urn:ietf:params:scim:schemas:core:2.0:ServiceProviderConfig"
-    ],
-    "documentation": "https://documenter.getpostman.com/view/1614368/RVfsHZ6o",
-    "patch": {
-      "supported": false
-    },
-    "bulk": {
-      "supported": false,
-      "maxOperations": 0,
-      "maxPayloadSize": 0
-    },
-    "filter": {
-      "supported": true,
-      "maxResults": 100
-    },
-    "changePassword": {
-      "supported": false
-    },
-    "sort": {
-      "supported": true
-    },
-    "etag": {
-      "supported": false
-    },
-    "authenticationSchemes": [
-      {
-        "type": "oauthbearertoken",
-        "name": "OAuth Bearer Token",
-        "description": "Authentication scheme using the OAuth Bearer Token Standard",
-        "specUri": "https://www.rfc-editor.org/info/rfc6750"
-      }
-    ],
-    "meta": {
-      "resourceType": "ServiceProviderConfig",
-      "location": "https://scimps.azurewebsites.net/api/ServiceProviderConfig"
+    
+$psbody=[pscustomobject]@{
+    schemas=@("urn:ietf:params:scim:schemas:core:2.0:$($ServiceProviderConfig.PartitionKey)")
+}
+foreach ($prop in $ServiceProviderConfig.getenumerator()){
+    if ($prop.name -in ('PartitionKey','RowKey','Timestamp')){continue}
+    if ($prop.name -like "*_*"){
+        $tree="$($prop.name)".split('_')
+        #write-host "$($tree[0]) - $($tree[1]) - $(($psbody."$($tree[0])") -eq $null)"
+        if (($psbody."$($tree[0])") -ne $null){
+            $psbody."$($tree[0])" | add-member -notepropertyname "$($tree[1])" -notepropertyvalue ($prop.value) -verbose
+        }else{
+            $psbody | add-member -notepropertyname "$($tree[0])" -notepropertyvalue ([pscustomobject]@{"$($tree[1])"=$prop.value}) -verbose
+        }
+    }else{
+        #write-host ($prop | out-string)
+        #write-host "key=$($prop.name)"
+        $psbody | add-member -notepropertyname $prop.name -notepropertyvalue $prop.value -verbose
+    }   
+}
+$psauthenticationSchemes=@()
+foreach ($auth in $authenticationSchemes.getenumerator()){
+    $authscheme=[pscustomobject]@{}
+    write-host "auth - $auth"
+    foreach ($prop in $auth.keys){
+        if ($prop -in ('PartitionKey','RowKey','Timestamp')){continue}
+        $authscheme | add-member -notepropertyname "$prop" -notepropertyvalue $auth.$prop -verbose
     }
-  }'
-# Associate values to output bindings by calling 'Push-OutputBinding'. test
+    $psauthenticationSchemes+=$authscheme
+}
+$meta=[pscustomobject]@{
+    resourceType=$ServiceProviderConfig.PartitionKey
+    location="https://scimps.azurewebsites.net/api/ServiceProviderConfig"
+}
+$psbody | add-member -notepropertyname 'authenticationSchemes' -notepropertyvalue $psauthenticationSchemes
+$psbody | add-member -notepropertyname 'meta' -notepropertyvalue $meta
+
+# Associate values to output bindings by calling 'Push-OutputBinding'.
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
     StatusCode = $status
-    Body = $body
+    Body = $psbody
 })
