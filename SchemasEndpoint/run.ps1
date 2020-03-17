1f83e1122f9a5c2d83e583871defea09a43dc83d
410sean <410sean@gmail.com>
2020-03-17 13:55:00 -0400

    fix schema loop

--- SchemasEndpoint/run.ps1
+++ SchemasEndpoint/run.ps1
@@ -8,146 +8,7 @@ Write-Host "PowerShell HTTP trigger function processed a request."
 
 
 $status = [HttpStatusCode]::OK
-$body='{
-  "totalResults": 1,
-  "itemsPerPage": 100,
-  "startIndex": 1,
-  "schemas": [
-    "urn:ietf:params:scim:api:messages:2.0:ListResponse"
-  ],
-  "Resources": [
-    {
-      "id": "urn:ietf:params:scim:schemas:core:2.0:User",
-      "name": "User",
-      "description": "Front teammate Account",
-      "attributes": [
-        {
-          "name": "userName",
-          "description": "Teammate''s alias. REQUIRED.",
-          "type": "string",
-          "multiValued": false,
-          "required": true,
-          "caseExact": false,
-          "mutability": "readWrite",
-          "returned": "default",
-          "uniqueness": "server"
-        },
-        {
-          "name": "displayName",
-          "description": "The name of the Teammate, suitable for display to end-users. UNSUPPORTED",
-          "type": "string",
-          "multiValued": false,
-          "required": false,
-          "caseExact": false,
-          "mutability": "readWrite",
-          "returned": "default",
-          "uniqueness": "none"
-        },
-        {
-          "name": "companyCode",
-          "description": "The company code of the user",
-          "type": "string",
-          "multiValued": false,
-          "required": false,
-          "caseExact": false,
-          "mutability": "readWrite",
-          "returned": "default",
-          "uniqueness": "none"
-        },
-        {
-          "name": "EmployeeClass",
-          "description": "the Employee Class of the user",
-          "type": "string",
-          "multiValued": false,
-          "required": false,
-          "caseExact": false,
-          "mutability": "readWrite",
-          "returned": "default",
-          "uniqueness": "none"
-        },
-        {
-          "name": "departmentCode",
-          "description": "the Department Code of the user",
-          "type": "string",
-          "multiValued": false,
-          "required": false,
-          "caseExact": false,
-          "mutability": "readWrite",
-          "returned": "default",
-          "uniqueness": "none"
-        },
-        {
-          "name": "BusinessUnitCode",
-          "description": "the Business Unit code, product code, or division code of the user",
-          "type": "string",
-          "multiValued": false,
-          "required": false,
-          "caseExact": false,
-          "mutability": "readWrite",
-          "returned": "default",
-          "uniqueness": "none"
-        },
-        {
-          "name": "jobCode",
-          "description": "the job code of the user",
-          "type": "string",
-          "multiValued": false,
-          "required": false,
-          "caseExact": false,
-          "mutability": "readWrite",
-          "returned": "default",
-          "uniqueness": "none"
-        },
-        {
-          "name": "domainID",
-          "description": "the domain override of the user",
-          "type": "string",
-          "multiValued": false,
-          "required": false,
-          "caseExact": false,
-          "mutability": "readWrite",
-          "returned": "default",
-          "uniqueness": "none"
-        },
-        {
-          "name": "location",
-          "description": "the office ID of the user",
-          "type": "string",
-          "multiValued": false,
-          "required": false,
-          "caseExact": false,
-          "mutability": "readWrite",
-          "returned": "default",
-          "uniqueness": "none"
-        },
-        {
-          "name": "role",
-          "description": "the returned role of the user",
-          "type": "string",
-          "multiValued": false,
-          "required": false,
-          "caseExact": false,
-          "mutability": "read",
-          "returned": "default",
-          "uniqueness": "none"
-        },
-        {
-          "name": "active",
-          "description": "A Boolean value indicating the Teammate''s administrative status. (Default to true)",
-          "type": "boolean",
-          "multiValued": false,
-          "required": false,
-          "mutability": "readWrite",
-          "returned": "default"
-        }
-      ],
-      "meta": {
-        "resourceType": "Schema",
-        "location": "https://scimps.azurewebsites.net/api/Schemas/urn:ietf:params:scim:schemas:core:2.0:User"
-      }
-    }
-  ]
-}'
+
 if ($Request.params.path){
   $targetschema=($schemas.where{"urn:ietf:params:scim:schemas:core:2.0:$($_.name)" -eq $Request.params.path})[0]
   $targetattributes=($schemaAttributes.where{$_.PartitionKey -eq $targetschema.name})
@@ -178,7 +39,7 @@ if ($Request.params.path){
     Resources=@()
   }
   $resources=@()
-  foreach ($targetschema in $resources){
+  foreach ($targetschema in $Schemas){
     $targetattributes=($schemaAttributes.where{$_.PartitionKey -eq $targetschema.name})
     $schemabody=[pscustomobject]@{
       id="urn:ietf:params:scim:schemas:core:2.0:$($targetschema.name)"
