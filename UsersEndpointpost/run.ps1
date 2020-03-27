d481b0b6ce12f893b586105706b6b94add2ffbe8
McGovern <smcgover@allegisgroup.com>
2020-03-27 15:17:37 -0400

    post check

--- UsersEndpointpost/run.ps1
+++ UsersEndpointpost/run.ps1
@@ -4,6 +4,21 @@ using namespace System.Net
 param($Request, $TriggerMetadata, $Schemas, $schemaAttributes)
 Write-Verbose ($request | convertto-json -depth 10) -verbose
 write-host ($request | convertto-json -depth 10) 
+function new-scimuser ($prop){
+    $userobj=[pscustomobject]@{
+        schemas=@('urn:ietf:params:scim:schemas:core:2.0:User')
+        id=$prop.RowKey
+    }
+    foreach ($attr in $schemaAttributes.name.where{$_ -notin ('schemas','id')}){
+        if ($prop.$attr){$userobj | add-member -notepropertyname $attr -notepropertyvalue $prop.$attr}
+    }
+    $meta=[pscustomobject]@{
+        resourceType='User'
+        location="https://scimps.azurewebsites.net/api/Users/$($prop.RowKey)"
+    }
+    $userobj | add-member -notepropertyname meta -notepropertyvalue $meta
+    return $userobj
+}
 $status = [HttpStatusCode]::OK
 $guid=(new-guid).guid
 $myvalue=[pscustomobject]@{
@@ -11,19 +26,19 @@ $myvalue=[pscustomobject]@{
     RowKey=$guid
 }
 $userjson=$Request.Body | convertfrom-json
-write-host "parsing $($Request.Body | convertto-json -depth 10)"
-write-host "parsing $($Request.Body))"
+#write-host "parsing $($Request.Body | convertto-json -depth 10)"
+#write-host "parsing $($Request.Body))"
 write-host "parsing $($userjson.displayName)"
 foreach ($attr in $schemaAttributes.where{$_.PartitionKey -eq 'User'}.name){
-    write-host "checking for $attr=$($Request.Body.$attr)"
-    if ($Request.Body.$attr){$myvalue | add-member -notepropertyname $attr -notepropertyvalue $Request.Body.$attr}
+    write-host "checking for $attr=$($userjson.$attr)"
+    if ($userjson.$attr){$myvalue | add-member -notepropertyname $attr -notepropertyvalue $userjson.$attr}
 }
 Push-OutputBinding -Name createUser -Value $myValue
 #$result=Invoke-RestMethod -Uri "$($Request.url)/$guid" -Method Get
 #if ($result.schemas[0] -eq 'urn:ietf:params:scim:schemas:core:2.0:User'){
 #    $body=$result
 #}else{
-    $body=$myvalue
+    $body=new-scimuser $myvalue
 #}
 write-verbose $body -Verbose
 $status = [HttpStatusCode]::OK
