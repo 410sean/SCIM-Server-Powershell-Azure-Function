839ca8c50686211f89eef10abc277270a8ae6235
McGovern <smcgover@allegisgroup.com>
2020-03-27 15:27:20 -0400

    status code always created on post

--- UsersEndpointpost/run.ps1
+++ UsersEndpointpost/run.ps1
@@ -41,7 +41,7 @@ Push-OutputBinding -Name createUser -Value $myValue
     $body=new-scimuser $myvalue
 #}
 write-verbose $body -Verbose
-$status = [HttpStatusCode]::OK
+$status = [HttpStatusCode]::Created
 # Associate values to output bindings by calling 'Push-OutputBinding'.
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
     StatusCode = $status
