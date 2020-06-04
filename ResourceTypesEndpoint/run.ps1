b19f8a7f9c30fee6184d5135599b94aa85ebaddb
Sean McGovern <410sean@gmail.com>
2020-06-04 13:06:26 -0400

    readd new-scimitem to resourcetype

--- ResourceTypesEndpoint/run.ps1
+++ ResourceTypesEndpoint/run.ps1
@@ -6,7 +6,34 @@ write-host ($request | convertto-json -depth 10)
 write-host ($TriggerMetadata | convertto-json -depth 10) 
 # Write to the Azure Functions log stream.
 Write-Host "PowerShell HTTP trigger function processed a request."
-
+function new-scimItem ($schema, $properties, $location, [switch]$includeMeta){
+  #will create item starting with schema and ending with meta
+  #nested properties should have a property name that can be split by underscore '_'
+  $psitem=[pscustomobject]@{
+    schemas=@("urn:ietf:params:scim:schemas:core:2.0:$schema")
+  }
+  foreach ($prop in $properties){
+    if ($prop.name -in ('PartitionKey','RowKey','Timestamp')){continue}
+    if ($prop.name -like "*_*"){
+        $tree="$($prop.name)".split('_')
+        if ($null -ne ($psitem."$($tree[0])")){
+            $psitem."$($tree[0])" | add-member -notepropertyname "$($tree[1])" -notepropertyvalue ($prop.value) -verbose
+        }else{
+            $psitem | add-member -notepropertyname "$($tree[0])" -notepropertyvalue ([pscustomobject]@{"$($tree[1])"=$prop.value}) -verbose
+        }
+    }else{
+        $psitem | add-member -notepropertyname $prop.name -notepropertyvalue $prop.value -verbose
+    }
+  }
+  if ($includemeta){
+      $meta=[pscustomobject]@{
+          resourceType=$schema
+          location="$location"
+      }
+      $psitem | add-member -notepropertyname 'meta' -notepropertyvalue $meta
+  }
+  return $psitem
+}
 $status = [HttpStatusCode]::OK
   if ($Request.params.path -and $Request.params.path.length -ne 0){
     $targetresource=($ResourceType.where{$_.name -eq $Request.params.path})[0]
@@ -33,5 +60,4 @@ write-host ($psbody | convertto-json -depth 10)
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
     StatusCode = $status
     Body = $psbody
-    headers = @{"Content-Type"= "application/json"}
 })
