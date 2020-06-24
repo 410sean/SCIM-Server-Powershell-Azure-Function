2dc931e3b957782b2a9113dbe335c342371aef2e
Sean McGovern <410sean@gmail.com>
2020-06-24 16:22:32 -0400

    fix get users link

--- UsersEndpointget/run.ps1
+++ UsersEndpointget/run.ps1
@@ -36,12 +36,12 @@ $response=get-scimUser @params
 if ($response.schemas -contains 'urn:ietf:params:scim:api:messages:2.0:ListResponse')
 {
   foreach ($resource in $response.resources){
-    write-host "setting '$($response.schemas)' meta location $("https://$($Request.Headers.'disguised-host')/api/Users/$($resource.name)")"
-    $resource.meta.location="https://$($Request.Headers.'disguised-host')/api/Users/$($resource.name)"
+    write-host "setting '$($response.schemas)' meta location $("https://$($Request.Headers.'disguised-host')/api/Users/$($resource.id)")"
+    $resource.meta.location="https://$($Request.Headers.'disguised-host')/api/Users/$($resource.id)"
   }
 }elseif($response.schemas -contains 'urn:ietf:params:scim:schemas:core:2.0:User'){
-  write-host "setting '$($response.schemas)' meta location $("https://$($Request.Headers.'disguised-host')/api/Users/$($resource.name)")"
-  $response.meta.location="https://$($Request.Headers.'disguised-host')/api/Users/$($response.name)" 
+  write-host "setting '$($response.schemas)' meta location $("https://$($Request.Headers.'disguised-host')/api/Users/$($resource.id)")"
+  $response.meta.location="https://$($Request.Headers.'disguised-host')/api/Users/$($response.id)" 
 }elseif($response.schemas -contains 'urn:ietf:params:scim:api:messages:2.0:Error'){
   $code=$response.status
   $status = [HttpStatusCode]::$code
