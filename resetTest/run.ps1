2f7467f63aa9ebd5698efa17de876ef41d979ce3
Sean McGovern <410sean@gmail.com>
2020-07-16 15:42:19 -0400

    added resettest

---
+++ resetTest/run.ps1
@@ -0,0 +1,30 @@
+using namespace System.Net
+
+# Input bindings are passed in via param block.
+param($Request, $TriggerMetadata)
+write-host ($request | convertto-json -depth 10) 
+write-host ($TriggerMetadata | convertto-json -depth 10) 
+$body=Test-BasicAuthCred -authorization ($request.Headers.Authorization)
+if ($null -ne $body){
+    Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+        StatusCode = [HttpStatusCode]::Unauthorized
+        Body = $body
+        headers = @{"Content-Type"= "application/scim+json"}
+    })
+    $keepgoing=$false
+}
+$users=get-scimUser -filter "PartitionKey eq 'User' and username eq '$($request.Params.path)'"
+foreach($id in $users.Resources.id){
+    $body=remove-scimUser -path $id
+}
+
+
+$status = [HttpStatusCode]::OK
+write-host ($status | convertto-json -depth 10) 
+write-host ($Body | convertto-json -depth 10) 
+# Associate values to output bindings by calling 'Push-OutputBinding'.
+Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+    StatusCode = $status
+    Body = $Body | convertto-json -depth 10
+    headers = @{"Content-Type"= "application/scim+json"}
+})
